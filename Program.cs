27cecea72c7ccf56023e27507aed30392b1d4163
The-X-ray-man <petouhoff@gmail.com>
2023-12-05 11:32:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Svetli_BaristaContest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
